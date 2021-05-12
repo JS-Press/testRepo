29589be35b5b72e72dab2b461c077891f8682222
Jarad Solomon <jaradsolomon@gmail.com>
2021-05-11 21:54:51 -0500

    switching worms around

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.querySelector('body').appendChild(h6);
 
 h6.addEventListener('click', function() {
    // alert('In a sec, JEEZ!');
-    worm.src = "./images/worm9.jpg"
+    worm.src = "./images/worm17.jpg"
     h6.textContent = "Mhm, And One More";
     h6.addEventListener('click', function() {
          worm.src = "./images/worm23.jpg"
@@ -25,10 +25,10 @@ h6.addEventListener('click', function() {
                         worm.src = "./images/worm15.jpg"
                         h6.textContent = "...And A Mouth...";
                         h6.addEventListener('click', function() {
-                            worm.src = "./images/worm20.jpg"
+                            worm.src = "./images/worm9.jpg"
                             h6.textContent = "Or Is It A Rabbit?";
                             h6.addEventListener('click', function() {
-                                worm.src = "./images/worm17.jpg"
+                                worm.src = "./images/worm20.jpg"
                                 h6.textContent = "With Ears?";
                                 h6.addEventListener('click', function() {
                                     worm.src = "./images/worm8.jpg"
@@ -50,10 +50,12 @@ h6.addEventListener('click', function() {
                                                         h6.textContent = "There's this one";
                                                         h6.addEventListener('click', function() {
                                                             worm.src = "./images/worm24.jpg"
-                                                            h6.textContent = "This one";
+                                                            h6.textContent = "This one...";
                                                             h6.addEventListener('click', function() {
                                                                 worm.src = "./images/worm25.jpg"
                                                                 h6.textContent = "And This Last one";
+                                                                
+
                                                               });
                                                           });
                                                       });
